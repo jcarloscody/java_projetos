7482d2927425c0248fd9af65151e6f5ef27079b2
Josue <josuecarlosos2@gmail.com>
2022-01-06 17:43:40 -0300

    criacao do repositorio e service da categoria e o metodo com o endpoit para buscar um item

---
+++ SpringBoot/winner/src/main/java/com/conquistandoavaga/winner/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.conquistandoavaga.winner.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.conquistandoavaga.winner.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
--- SpringBoot/winner/src/main/java/com/conquistandoavaga/winner/resources/CategoriaResource.java
+++ SpringBoot/winner/src/main/java/com/conquistandoavaga/winner/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.conquistandoavaga.winner.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.conquistandoavaga.winner.domain.Categoria;
+import com.conquistandoavaga.winner.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "funcionando /categories get";
+	@Autowired
+	CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria categoria = service.buscar(id);
+		return ResponseEntity.ok().body(categoria);
 	}
 }
