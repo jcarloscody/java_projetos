ff86ec471630d519da59f5ffc3622e8cd91952c3
Josue <josuecarlosos2@gmail.com>
2022-01-07 18:06:33 -0300

    adicionando a class Page - paginacao para o front end

--- SpringBoot/winner/src/main/java/com/conquistandoavaga/winner/resources/CategoriaResource.java
+++ SpringBoot/winner/src/main/java/com/conquistandoavaga/winner/resources/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -68,4 +70,21 @@ public class CategoriaResource {
 		List<CategoriaDTO> categoriaDTO = categoria.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(categoriaDTO);
 	}
+	
+	
+	///categories/page?linesPerPage=1&page1
+	@RequestMapping(value = "page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+		@RequestParam(value = "page", defaultValue = "0") Integer page, 
+		@RequestParam(value = "linesPerPage", defaultValue = "2")  Integer linesPerPage, 
+		@RequestParam(value = "orderBy", defaultValue = "nome")  String orderBy, 
+		@RequestParam(value = "direction", defaultValue = "ASC")  String direction) {
+		
+		Page<Categoria> categoria = service.findPage(page, linesPerPage, orderBy, direction);
+		 
+		 //usando o DTO 
+		Page<CategoriaDTO> categoriaDTO = categoria.map(obj -> new CategoriaDTO(obj));
+		return ResponseEntity.ok().body(categoriaDTO);
+	}
+	
 }
--- SpringBoot/winner/src/main/java/com/conquistandoavaga/winner/services/CategoriaService.java
+++ SpringBoot/winner/src/main/java/com/conquistandoavaga/winner/services/CategoriaService.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.conquistandoavaga.winner.domain.Categoria;
@@ -49,5 +52,12 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repository.findAll();
+	}
+	
+	//Page - class do springboot que encapsula informacoes e operacoes sobre a paginacao
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repository.findAll(pageRequest);
 	}
 }
