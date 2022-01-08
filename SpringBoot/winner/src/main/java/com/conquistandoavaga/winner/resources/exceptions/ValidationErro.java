4f11ee419fce157c8d1a6f0e9ecf20a028d9540d
Josue <josuecarlosos2@gmail.com>
2022-01-07 22:00:41 -0300

    erro personalizado no resourceExceptionHandler MethodArgumentNotValidException

---
+++ SpringBoot/winner/src/main/java/com/conquistandoavaga/winner/resources/exceptions/ValidationErro.java
@@ -0,0 +1,23 @@
+package com.conquistandoavaga.winner.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationErro extends StandardError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> list = new ArrayList<>();
+	
+	public ValidationErro(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+	
+	public List<FieldMessage> getList(){
+		return list;
+	}
+	
+	public void addError(String fieldName, String message) {
+		list.add(new FieldMessage(fieldName, message));
+	}
+
+}
